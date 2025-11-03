fc90959c8ea26f56859150a299567247de796a7f
Oatu Mihai <mihai.oatu@student.unitbv.ro>
2025-11-03 13:39:02 +0200

    Modificari la clasa FitZoneManager la metodele de citire+adaugare si finalizare clasa main

--- Tema1/src/main/java/org/example/FitZoneManager.java
+++ Tema1/src/main/java/org/example/FitZoneManager.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FitZoneManager {
     private List<Antrenor> listaAntrenori;
@@ -14,31 +15,79 @@ public class FitZoneManager {
         listaClaseAntrenament = new ArrayList<>();
     }
 
-    public void adaugaAntrenor(Antrenor antrenor) {
+    public void adaugaAntrenor() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduceti numele antrenorului: ");
+        String nume = scanner.nextLine();
+        System.out.print("Introduceti tipul clasei predate: ");
+        String tipClasa = scanner.nextLine();
+        System.out.print("Este antrenor angajat full-time? (da/nu): ");
+        String status = scanner.nextLine();
+
+        Antrenor antrenor;
+        if (status.equals("da")) {
+            System.out.print("Introduceti salariul lunar al antrenorului: ");
+            double salariuLunar = scanner.nextDouble();
+            System.out.print("Introduceti vechimea in ani a antrenorului: ");
+            int vechimeAni = scanner.nextInt();
+            antrenor = new AntrenorPermanent(nume, tipClasa, salariuLunar, vechimeAni);
+        } else {
+            System.out.print("Introduceti tariful pe ora al antrenorului: ");
+            double tarifPeOra = scanner.nextDouble();
+            System.out.print("Introduceti numarul de ore contractate lunar: ");
+            int nrOreContractateLunar = scanner.nextInt();
+            antrenor = new ColaboratorExtern(nume, tipClasa, tarifPeOra, nrOreContractateLunar);
+        }
         listaAntrenori.add(antrenor);
     }
-    public void adaugaAbonament(Abonament abonament) {
+    public void adaugaAbonament() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduceti tipul abonamentului (Standard/Premium): ");
+        String tipAbonament = scanner.nextLine();
+        System.out.println("Introduceti durata abonamentului(Luni): ");
+        int durataLuni = scanner.nextInt();
+        System.out.print("Introduceti pretul de baza al abonamentului: ");
+        double pretBaza = scanner.nextDouble();
+
+        Abonament abonament;
+        if (tipAbonament.equals("Standard")) {
+            abonament = new AbonamentStandard(pretBaza, durataLuni);
+        } else {
+            abonament = new AbonamentPremium(pretBaza, durataLuni);
+        }
         listaAbonamente.add(abonament);
+
     }
-    public void adaugaClasaAntrenament(ClasaAntrenament clasaAntrenament) {
-        listaClaseAntrenament.add(clasaAntrenament);
+    public void adaugaClasaAntrenament() {
+        System.out.println("Introduceti numele clasei: ");
+        Scanner scanner = new Scanner(System.in);
+        String numeClasa = scanner.nextLine();
+        System.out.println("Introduceti nivelul de intensitate (Incepator/Intermediar/Avansat): ");
+        String nivelIntensitate = scanner.nextLine();
+        System.out.println("Introduceti pretul de baza al clasei: ");
+        double pretDeBaza = scanner.nextDouble();
+        ClasaAntrenament clasa = new ClasaAntrenament(numeClasa, nivelIntensitate, pretDeBaza);
+        listaClaseAntrenament.add(clasa);
     }
 
-    public void afiseazaToateDetaliile() {
+    public void afisareAntrenori() {
         System.out.println("---- Antrenori ----");
         for (Antrenor antrenor : listaAntrenori) {
             antrenor.afiseazaDetalii();
             System.out.println("Status Angajare: " + antrenor.getStatusAngajare());
             System.out.println();
         }
-
+    }
+    public void afisareAbonamente() {
         System.out.println("---- Abonamente ----");
         for (Abonament abonament : listaAbonamente) {
             abonament.afiseazaDetalii();
             System.out.println("Cost Total: " + abonament.calculeazaCostTotal());
             System.out.println();
         }
+    }
 
+    public void afisareClaseAntrenament() {
         System.out.println("---- Clase de Antrenament ----");
         for (ClasaAntrenament clasa : listaClaseAntrenament) {
             clasa.afiseazaDetaliiClasa();
--- Tema1/src/main/java/org/example/Main.java
+++ Tema1/src/main/java/org/example/Main.java
@@ -1,7 +1,62 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        FitZoneManager manager = new FitZoneManager();
+        Scanner scanner = new Scanner(System.in);
+        int optiune;
+
+        do{
+            afiseazaMeniu();
+            System.out.print("Alege o optiune: ");
+            optiune = scanner.nextInt();
+            scanner.nextLine();
+            proceseazaOptiune(optiune, manager, scanner);
+        }while(optiune != 0);
+        scanner.close();
+        System.out.println("Aplicatia s-a inchis.");
+    }
+    private static void afiseazaMeniu() {
+        System.out.println("\n--- Meniu FitZone ---");
+        System.out.println("1. Adauga Antrenor");
+        System.out.println("2. Adauga Abonament");
+        System.out.println("3. Adauga Clasa de Antrenament");
+        System.out.println("4. Genereaza Raport Sumar");
+        System.out.println("5. Afisare Abonamente");
+        System.out.println("6. Afisare Antrenori");
+        System.out.println("7. Afisare Clase de Antrenament");
+        System.out.println("0. Iesire");
+    }
+
+    private static void proceseazaOptiune(int optiune, FitZoneManager manager, Scanner scanner) {
+        switch (optiune) {
+            case 1:
+                manager.adaugaAntrenor();
+                break;
+            case 2:
+                manager.adaugaAbonament();
+                break;
+            case 3:
+                manager.adaugaClasaAntrenament();
+                break;
+            case 4:
+                manager.genereazaRaportSumar();
+                break;
+            case 5:
+                manager.afisareAbonamente();
+                break;
+            case 6:
+                manager.afisareAntrenori();
+                break;
+            case 7:
+                manager.afisareClaseAntrenament();
+                break;
+            case 0:
+                break;
+            default:
+                System.out.println("Optiune invalida. Te rog incearca din nou.");
+        }
     }
 }
